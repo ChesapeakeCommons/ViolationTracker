97cd3260ff7c21b1bc242f77c8c5e5ab9f4f84ea
Ivan Trajkovic <ivan.ebiz@gmail.com>
2022-08-24 23:47:23 +0200

    updates to Airflow DAG  for worker - chnaged docker connection name, container has a name now, and DAG object explicitly passed to tasks.

--- Worker/airflow/cm_violationtracker_worker_dag.py
+++ Worker/airflow/cm_violationtracker_worker_dag.py
@@ -8,6 +8,7 @@ from airflow import DAG
 # from airflow.operators.python_operator import PythonOperator
 from airflow.operators.docker_operator import DockerOperator
 
+docker_conn_id_str  = "docker_aws_erc"
 docker_host_url_str = "tcp://localhost:2376" # "unix://var/run/docker.sock"
 
 # #-----------------------------------------------------------------
@@ -17,7 +18,7 @@ docker_host_url_str = "tcp://localhost:2376" # "unix://var/run/docker.sock"
 #     return cm_mapbox_api_token_str
 
 #-----------------------------------------------------------------
-with DAG(
+dag = DAG(
     'cm_violation_tracker_worker',
 
     # These args will get passed on to each operator
@@ -46,37 +47,46 @@ with DAG(
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 1, 1),
     catchup=False,
-    tags=['cm'],
+    tags=['cm'])
+
+# DOCKER_OPERATOR
+t1 = DockerOperator(
+    task_id='R_worker',
+    image='833394423843.dkr.ecr.us-east-1.amazonaws.com/violationtracker_worker:dev',
+    container_name="cm_violationtracker_r_worker",
+
+    # command="/bin/sleep 10",
+
+    environment={
+        # "AWS_DEFAULT_REGION": "us-east-1",
+        "MB_API_TOKEN": "{{ var.value.get('cm_mapbox_api_token_dev', 'undefined') }}",
+        "WR_API_TOKEN": "{{ var.value.get('cm_waterreporter_api_key_dev', 'undefined') }}"
+    },
     
-) as dag:
+    api_version='auto',
 
-    # DOCKER_OPERATOR
-    t1 = DockerOperator(
-        task_id='R_worker',
-        image='833394423843.dkr.ecr.us-east-1.amazonaws.com/violationtracker_worker:dev',
-        # command="/bin/sleep 10",
+    # Allows to remove the Docker container as soon as the task is finished
+    auto_remove=True,
 
-        environment={
-            # "AWS_DEFAULT_REGION": "us-east-1",
-            "MB_API_TOKEN": "{{ var.value.get('cm_mapbox_api_token_dev', 'undefined') }}",
-            "WR_API_TOKEN": "{{ var.value.get('cm_waterreporter_api_key_dev', 'undefined') }}"
-        },
-        
-        api_version='auto',
+    # always try to pull the latest version of the container, in case the image
+    # got updated by a build system or manually by a developer.
+    force_pull=True,
 
-        # Allows to remove the Docker container as soon as the task is finished
-        auto_remove=True,
+    docker_url=docker_host_url_str,
 
-        # always try to pull the latest version of the container, in case the image
-        # got updated by a build system or manually by a developer.
-        force_pull=True,
+    #--------------------------
+    # AIRFLOW_CONNECTION
+    # ID of the Airflow connection to use
+    docker_conn_id=docker_conn_id_str,
 
-        docker_url=docker_host_url_str,
+    #--------------------------
 
-        # keep the worker container network isolated from the host it runs on
-        network_mode="bridge",
-        
-        # MEMORY_CONSTRAINT
-        # comment out if you dont want to limit how much memory this container is allowed
-        # to consume at worst.
-        mem_limit="1gb")
+    # keep the worker container network isolated from the host it runs on
+    network_mode="bridge",
+    
+    # MEMORY_CONSTRAINT
+    # comment out if you dont want to limit how much memory this container is allowed
+    # to consume at worst.
+    mem_limit="1gb",
+    
+    dag=dag)
